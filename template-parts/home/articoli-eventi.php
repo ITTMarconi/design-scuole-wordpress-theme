89dabf016c143254e283f42531cfdd61af95cb48
giovannicaini <79083704+giovannicaini@users.noreply.github.com>
2023-07-07 14:37:02 +0200

    fix: view news with single typology

--- template-parts/home/articoli-eventi.php
+++ template-parts/home/articoli-eventi.php
@@ -40,6 +40,44 @@ if (is_array($tipologie_notizie) && count($tipologie_notizie)) {
                     if ($ct >= $column)
                         break;
 
+        $tipologia_notizia = get_term_by("id", $id_tipologia_notizia, "tipologia-articolo");
+        if($tipologia_notizia) {
+            // se è selezionata solo una tipologia, pesco 2 elementi
+            $ppp=1;
+            if((count($tipologie_notizie) == 1) && ($home_show_events == "false")){
+                $ppp=2;
+                echo '<div class="row variable-gutters">';
+            }
+            $args = array('post_type' => 'post',
+                    'posts_per_page' => $ppp,
+                    'tax_query' => array(
+                        array(
+                            'taxonomy' => 'tipologia-articolo',
+                            'field' => 'term_id',
+                            'terms' => $tipologia_notizia->term_id,
+                        ),
+                    ),
+                );
+            $posts = get_posts($args);
+
+            $lg = 4;
+            if((count($tipologie_notizie) == 1) && ($home_show_events == "false"))
+                $lg = 8;
+            if (is_array($posts) && count($posts)) {
+            ?>
+            <div class="col-lg-<?php echo $lg; ?>">
+                <div class="title-section pb-4">
+                    <h2><?php echo $tipologia_notizia->name; ?></h2>
+                </div><!-- /title-section -->
+
+                <?php
+                if((count($tipologie_notizie) == 1) && ($home_show_events == "false"))
+                    echo '<div class="row variable-gutters">';
+                foreach ($posts as $post) {
+                    if((count($tipologie_notizie) == 1) && ($home_show_events == "false"))
+                        echo '<div class="col-lg-6 mb-2">';
+                    get_template_part("template-parts/single/card", "vertical-thumb");
+
                     $tipologia_notizia = get_term_by("id", $id_tipologia_notizia, "tipologia-articolo");
                     if ($tipologia_notizia) {
                         // se è selezionata solo una tipologia, pesco 2 elementi
