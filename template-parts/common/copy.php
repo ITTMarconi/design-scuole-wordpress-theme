67d869f3a786e52a8566073aeb08ce8cc56076b1
mlongano <mauro.longano@marconirovereto.it>
2023-07-20 11:22:49 +0200

    Add credits to ITT Marconi web team

--- template-parts/common/copy.php
+++ template-parts/common/copy.php
@@ -2,4 +2,7 @@
     <div class="col-lg-10 offset-lg-1 text-right footer-link">
         <a href="https://designers.italia.it/kit/scuole/" aria-label="Concept &amp; Design by Designers Italia" target="_blank">Concept & Design by Designers Italia</a>
     </div>
+    <div class="col-lg-10 offset-lg-1 text-right footer-link">
+        <a href="/struttura/dirigente-scolastico/web-team" aria-label="Adattamento a cura del team dell'ITT Marconi" target="_blank">Adattamento a cura del team dell'<span>ITT Marconi</span></a>
+    </div>
 </div><!-- /row -->
