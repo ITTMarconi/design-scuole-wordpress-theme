18a4642aedf5cd4915e3606f8059723ff6d80b9e
mlongano <mauro.longano@marconirovereto.it>
2023-07-23 22:24:58 +0200

    Use custom properties for colors and fix hero

--- assets/js/marconi/marconi.js
+++ assets/js/marconi/marconi.js
@@ -1,40 +1,34 @@
 console.log("Enter Marconi Script");
 
-jQuery('#hero-container').addClass('absolute-orizontal-position');
+function showHero(evt) {
+  //evt.preventDefault();
+  evt.stopPropagation();
+  jQuery('.hero-title').removeClass('sr-only sr-only-focusable');
+	console.log("SHOW Hero");
+}
 
-jQuery('#hero-container .container .hero-title .btn').on('mouseenter touchstart', function(e) {
-	e.stopPropagation();
-});
-jQuery('#hero-container .hero-title .btn').on('mouseenter touchend', function(e) {
-	e.stopPropagation();
-});
-jQuery('#hero-container .hero-title .btn').on('click', function(e) {
-	e.stopPropagation();
-});
+function hideHero(evt) {
+  jQuery('.hero-title').addClass('sr-only sr-only-focusable');
+	console.log("HIDE Hero");
+}
 
-
-jQuery('.section-hero-left').on('click', function(e) {
-	e.stopPropagation();
-	jQuery('#hero-container').removeClass('sr-only sr-only-focusable');
+// Rendi il bottone all'interno di #hero-container cliccabile
+jQuery('.section-hero-marconi .hero-title .btn').on('touchstart', function(e) {
+	//e.stopPropagation();
 });
-
-jQuery('.section-hero-left').on('mouseenter touchstart', function(e) {
-	e.stopPropagation();
-	jQuery('#hero-container').removeClass('sr-only sr-only-focusable');
-});
-jQuery('.section-hero-left').on('mouseleave touchend', function(e) {
-	e.stopPropagation();
-	jQuery('#hero-container').addClass('sr-only sr-only-focusable');
+jQuery('.section-hero-marconi .hero-title .btn').on('click', function(e) {
+	//e.stopPropagation();
 });
 
-jQuery('#main-container').on('click', function(e) {
-	jQuery('#hero-container').addClass('sr-only sr-only-focusable');
-});
-jQuery('#main-container').on('mouseenter touchstart', function(e) {
-	jQuery('#hero-container').addClass('sr-only sr-only-focusable');
-});
+// Visualizza il testo Hero se clicchi sull'immagine Hero
+jQuery('.section-hero-marconi').on('touchstart', showHero);
+jQuery('.section-hero-marconi').on('mouseenter', showHero);
+jQuery('.section-hero-marconi').on('mouseleave', hideHero);
+jQuery('.section-hero-marconi').on('click', showHero);
 
-
+// Nascondi il testo Hero se clicchi su la zona sotto (#main-container)
+jQuery('#main-container').on('click', hideHero);
+jQuery('#main-container').on('touchstart', hideHero);
 
 /*
  * 
