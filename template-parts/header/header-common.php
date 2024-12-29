01ef7c58506e49330259a9c724daf2ea90651847
mlongano <mauro.longano@marconirovereto.it>
2024-12-29 19:11:03 +0100

    fix(home): fix the layout the home messages and the small header

--- template-parts/header/header-common.php
+++ template-parts/header/header-common.php
@@ -1,6 +1,6 @@
 <button type="button" class="icon-text pr-2" aria-label="<?php _e("Servizi esterni", "design_scuole_italia"); ?>" data-toggle="modal" data-target="#services-modal">
     <span class="mr-2 d-none d-lg-block"><?php _e("Servizi esterni", "design_scuole_italia"); ?></span>
     <div class="icon-wrapper p-0 flex flex-nowrap justify-center items-center">
-        <svg class="svg-bookmark-solid"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bookmark-solid"></use></svg>
+        <svg class="svg-bookmark-solid  min-[992px]:translate-y-2"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-bookmark-solid"></use></svg>
     </div><!-- /icon-wrapper -->
 </button>
