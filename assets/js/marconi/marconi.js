f9ca94336fdcc3d1cddd5dd6257a4cc06b0e8af6
mlongano <mauro.longano@marconirovereto.it>
2026-03-23 22:21:04 +0000

    style(footer): add accordion functionality to archive and category widgets

--- assets/js/marconi/marconi.js
+++ assets/js/marconi/marconi.js
@@ -39,3 +39,97 @@ jQuery('.section-hero-left').on('mouseleave touchend', function() {
 });
 */
 
+/**
+ * Footer Archives & Categories Accordion
+ */
+jQuery(document).ready(function($) {
+	// Handle traditional widget_archive widgets
+	$('#footer-wrapper .widget_archive').each(function() {
+		var $widget = $(this);
+		var $heading = $widget.find('.widget-title, h2, h3').first();
+		var $list = $widget.find('ul').first();
+		
+		if ($heading.length && $list.length) {
+			setupAccordion($heading, $list);
+		}
+	});
+	
+	// Handle Gutenberg block archives (heading and list are siblings)
+	$('#footer-wrapper .wp-block-archives-list').each(function() {
+		var $list = $(this);
+		var $heading = $list.siblings('.wp-block-heading, h2, h3').first();
+		
+		if ($heading.length) {
+			setupAccordion($heading, $list);
+		}
+	});
+	
+	// Handle traditional widget_categories widgets
+	$('#footer-wrapper .widget_categories').each(function() {
+		var $widget = $(this);
+		var $heading = $widget.find('.widget-title, h2, h3').first();
+		var $list = $widget.find('ul').first();
+		
+		if ($heading.length && $list.length) {
+			setupAccordion($heading, $list);
+		}
+	});
+	
+	// Handle Gutenberg block categories (heading and list are siblings)
+	$('#footer-wrapper .wp-block-categories-list').each(function() {
+		var $list = $(this);
+		var $heading = $list.siblings('.wp-block-heading, h2, h3').first();
+		
+		if ($heading.length) {
+			setupAccordion($heading, $list);
+		}
+	});
+	
+	// Handle custom taxonomy widgets (tipologia-articolo)
+	$('#footer-wrapper .marconi-taxonomy-widget').each(function() {
+		var $widget = $(this);
+		var $heading = $widget.find('.wp-block-heading, h2, h3').first();
+		var $list = $widget.find('.wp-block-taxonomy-list, ul').first();
+		
+		if ($heading.length && $list.length) {
+			setupAccordion($heading, $list);
+		}
+	});
+	
+	// Handle Gutenberg block taxonomy lists (heading and list are siblings)
+	$('#footer-wrapper .wp-block-taxonomy-list').each(function() {
+		var $list = $(this);
+		var $heading = $list.siblings('.wp-block-heading, h2, h3').first();
+		
+		if ($heading.length) {
+			setupAccordion($heading, $list);
+		}
+	});
+	
+	function setupAccordion($heading, $list) {
+		// Make heading clickable
+		$heading.on('click', function() {
+			$(this).toggleClass('active');
+			$list.toggleClass('open');
+		});
+		
+		// Add keyboard accessibility
+		$heading.attr('tabindex', '0').attr('role', 'button').attr('aria-expanded', 'false');
+		$list.attr('aria-hidden', 'true');
+		
+		$heading.on('keydown', function(e) {
+			if (e.key === 'Enter' || e.key === ' ') {
+				e.preventDefault();
+				$(this).trigger('click');
+			}
+		});
+		
+		// Update aria attributes on toggle
+		$heading.on('click', function() {
+			var isOpen = $list.hasClass('open');
+			$(this).attr('aria-expanded', isOpen);
+			$list.attr('aria-hidden', !isOpen);
+		});
+	}
+});
+
