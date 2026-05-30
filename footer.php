055a0ccc94f150477f5eec7b460724bc22aa31c7
Mauro Longano <mauro.longano@marconirovereto.it>
2026-05-30 21:31:56 +0200

    fix(footer): remove redundant `list-inline-item` class from contact list items
    Clean up contact list entries by removing the unnecessary `list-inline-item` class, keeping only the necessary `inline-flex items-center gap-2` classes.
    
    Also remove the generated `tail.css` from .gitignore as it is no longer part of the project.

--- footer.php
+++ footer.php
@@ -156,7 +156,7 @@
                 <?php if($contatti_centralino || $contatti_PEO || $contatti_PEC) { ?>
                     <ul class="list-inline">
                         <?php if($contatti_centralino) { ?>
-                            <li class="list-inline-item inline-flex items-center gap-2">
+                            <li class="inline-flex items-center gap-2">
                                 <?php echo $icon_centralino; ?>
                                 <a class="text-underline-hover" href="tel:<?php echo str_replace(' ', '', $contatti_centralino); ?>" aria-label="numero del centralino">
                                     <?php echo $contatti_centralino; ?>
@@ -164,14 +164,14 @@
                             </li>
                         <?php } ?>
                         <?php if($contatti_fax) { ?>
-                            <li class="list-inline-item inline-flex items-center gap-2">
+                            <li class="inline-flex items-center gap-2">
                                 <?php echo $icon_fax; ?>
                                 <?php echo $contatti_fax; ?>
                             </li>
                         <?php } ?>
 
                         <?php if($contatti_PEO) { ?>
-                            <li class="list-inline-item inline-flex items-center gap-2">
+                            <li class="inline-flex items-center gap-2">
                                 <?php echo $icon_PEO; ?>
                                 <a class="text-underline-hover" href="mailto:<?php echo str_replace(' ', '', $contatti_PEO); ?>" aria-label="indirizzo email istituzionale dell\'Istituto">
                                     <?php echo $contatti_PEO; ?>
@@ -179,7 +179,7 @@
                             </li>
                         <?php } ?>
                         <?php if($contatti_PEC) { ?>
-                            <li class="list-inline-item inline-flex items-center gap-2">
+                            <li class="inline-flex items-center gap-2">
                                 <?php echo $icon_PEC; ?>
                                 <a class="text-underline-hover" href="mailto:<?php echo str_replace(' ', '', $contatti_PEC); ?>" aria-label="indirizzo email PEC">
                                     <?php echo $contatti_PEC; ?>
