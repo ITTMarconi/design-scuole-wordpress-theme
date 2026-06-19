33bdd00b72a2b7c36917a2333ffa6e75b0ea76c0
mlongano <mauro.longano@marconirovereto.it>
2026-06-19 22:19:06 +0000

    refactor(ui): armonizza filtri albo, ripara azioni Condividi, aggiungi Copia link
    - filtri albo: card, toggle .btn-scopri con token design system,
      font Titillium Web, pannelli a comparsa via hidden/aria
    - article-title: min-height fluido via clamp invece di 480px fisso;
      article-title-small senza riserva altezza
    - azioni Stampa/Condividi: ID modal unici per pagina, markup role
      corretto, sottomenu Condividi funzionante via JS
    - aggiunta azione "Copia link" con fallback prompt se clipboard bloccato
    - rimosso bullet/margini estranei negli elenchi del modal azioni

--- template-parts/single/actions.php
+++ template-parts/single/actions.php
@@ -18,12 +18,17 @@ if(is_singular()){
     $current_title = dsi_get_option("nome_scuola");
 }
 
+$actions_suffix = is_singular() && isset( $post->ID ) ? $post->ID : md5( $current_uri );
+$modal_id = 'modal-more-items-' . $actions_suffix;
+$share_id = 'social-share-' . $actions_suffix;
+$share_control_id = 'share-control-' . $actions_suffix;
+
 ?><div class="actions-wrapper actions-main">
-    <a class="toggle-actions" href="#" title="Vedi azioni" data-target="#modal-more-items" data-toggle="modal">
+    <a class="toggle-actions" href="#" title="Vedi azioni" data-target="#<?php echo esc_attr( $modal_id ); ?>" data-toggle="modal">
         <svg class="it-more-items"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-more-items"></use></svg>
         <span><?php _e("Stampa / Condividi", "design_scuole_italia"); ?></span>
     </a>
-    <div class="modal fade modal-actions" id="modal-more-items" tabindex="-1" role="dialog" aria-hidden="true">
+    <div class="modal fade modal-actions" id="<?php echo esc_attr( $modal_id ); ?>" tabindex="-1" role="dialog" aria-hidden="true">
         <div class="modal-dialog modal-dialog-centered" role="document">
             <div class="modal-content">
                 <div class="modal-body">
@@ -66,26 +71,32 @@ if(is_singular()){
                                 </a>
                             </li>
                             <li>
-                                <a class="list-item collapsed link-toggle" title="<?php _e("Condividi", "design_scuole_italia"); ?>" href="#social-share" data-toggle="collapse" aria-expanded="false" aria-controls="social-share" role="button"id="share-control"> 
+                                <a href="#" class="list-item left-icon marconi-copy-link" data-copy-url="<?php echo esc_attr( $current_uri ); ?>" title="<?php _e("Copia il link", "design_scuole_italia"); ?>">
+                                    <svg class="icon it-share"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-share"></use></svg>
+                                    <span><?php _e("Copia link", "design_scuole_italia"); ?></span>
+                                </a>
+                            </li>
+                            <li>
+                                <a class="list-item collapsed link-toggle marconi-share-toggle" title="<?php _e("Condividi", "design_scuole_italia"); ?>" href="#<?php echo esc_attr( $share_id ); ?>" aria-expanded="false" aria-controls="<?php echo esc_attr( $share_id ); ?>" role="button" id="<?php echo esc_attr( $share_control_id ); ?>"> 
                                     <svg class="icon it-share"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-share"></use></svg>
                                     <span><?php _e("Condividi", "design_scuole_italia"); ?></span>
                                     <svg class="icon icon-toggle svg-arrow-down-small"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#svg-arrow-down-small"></use></svg>
                                 </a>
-                                <ul class="link-sublist collapse" id="social-share" role="region" aria-labelledby="share-control">
+                                <ul class="link-sublist" id="<?php echo esc_attr( $share_id ); ?>" role="region" aria-labelledby="<?php echo esc_attr( $share_control_id ); ?>" hidden>
                                     <li>
-                                        <a class="list-item" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($current_uri); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Facebook" target="_blank">
+                                        <a class="list-item" href="https://www.facebook.com/sharer/sharer.php?u=<?php echo urlencode($current_uri); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Facebook" target="_blank" rel="noopener noreferrer">
                                             <svg class="icon it-social-facebook"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-facebook"></use></svg>
                                             <span>Facebook</span>
                                         </a>
                                     </li>
                                     <li>
-                                        <a class="list-item" href="http://twitter.com/share?text=<?php echo urlencode($current_title); ?>&url=<?php echo urlencode($current_uri); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Twitter"  target="_blank">
+                                        <a class="list-item" href="https://twitter.com/share?text=<?php echo urlencode($current_title); ?>&url=<?php echo urlencode($current_uri); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Twitter"  target="_blank" rel="noopener noreferrer">
                                             <svg class="icon it-social-twitter"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-twitter"></use></svg>
                                             <span>Twitter</span>
                                         </a>
                                     </li>
                                     <li>
-                                        <a class="list-item" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo urlencode($current_uri); ?>&title=<?php echo urlencode($current_title); ?>&source=<?php echo dsi_get_option("nome_scuola"); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Linkedin"  target="_blank">
+                                        <a class="list-item" href="https://www.linkedin.com/shareArticle?mini=true&url=<?php echo urlencode($current_uri); ?>&title=<?php echo urlencode($current_title); ?>&source=<?php echo dsi_get_option("nome_scuola"); ?>" title="<?php _e("Condividi su", "design_scuole_italia"); ?>: Linkedin"  target="_blank" rel="noopener noreferrer">
                                             <svg class="icon it-social-linkedin"><use xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#it-social-linkedin"></use></svg>
                                             <span>Linkedin</span>
                                         </a>
@@ -98,4 +109,59 @@ if(is_singular()){
             </div><!-- /modal-content -->
         </div><!-- /modal-dialog -->
     </div><!-- /modal -->
+    <script>
+        jQuery(function($) {
+            function marconiCopyText(text) {
+                if (navigator.clipboard && window.isSecureContext) {
+                    return navigator.clipboard.writeText(text);
+                }
+
+                var textarea = document.createElement('textarea');
+                textarea.value = text;
+                textarea.setAttribute('readonly', '');
+                textarea.style.position = 'fixed';
+                textarea.style.left = '-9999px';
+                document.body.appendChild(textarea);
+                textarea.select();
+
+                return new Promise(function(resolve, reject) {
+                    try {
+                        document.execCommand('copy') ? resolve() : reject();
+                    } catch (error) {
+                        reject(error);
+                    } finally {
+                        document.body.removeChild(textarea);
+                    }
+                });
+            }
+
+            $('.marconi-copy-link').off('click.marconiCopyLink').on('click.marconiCopyLink', function(event) {
+                event.preventDefault();
+                var $link = $(this);
+                var $label = $link.find('span').first();
+                var defaultLabel = $label.text();
+
+                marconiCopyText($link.data('copy-url')).then(function() {
+                    $label.text('<?php echo esc_js( __( "Link copiato", "design_scuole_italia" ) ); ?>');
+                    setTimeout(function() {
+                        $label.text(defaultLabel);
+                    }, 2000);
+                }).catch(function() {
+                    window.prompt('<?php echo esc_js( __( "Copia il link", "design_scuole_italia" ) ); ?>', $link.data('copy-url'));
+                });
+            });
+
+            $('.marconi-share-toggle').off('click.marconiShare').on('click.marconiShare', function(event) {
+                event.preventDefault();
+                var $toggle = $(this);
+                var $target = $($toggle.attr('href'));
+                if (!$target.length) {
+                    return;
+                }
+                var isOpening = $target.prop('hidden');
+                $target.prop('hidden', !isOpening);
+                $toggle.attr('aria-expanded', isOpening ? 'true' : 'false').toggleClass('active', isOpening);
+            });
+        });
+    </script>
     </div><!-- /actions-wrapper --><?php
